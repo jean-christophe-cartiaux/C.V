20a90b3a02cb6172c06a6084ebb1014d6c4ba4ca
Jean-Christophe Cartiaux <cartiaux.jc@gmail.com>
2024-08-19 13:21:04 +0200

    ajout section xP ecole

--- src/Components/ExperienceEcole/XpEcole.jsx
+++ src/Components/ExperienceEcole/XpEcole.jsx
@@ -2,9 +2,66 @@ import React,{useState} from 'react'
 import'./XpEcole.css'
 
 const XpEcole = () => {
+    const [editMode,setEditMode]=useState(false);
+    const [ecole,setEcole]=useState([
+        {
+           titre:'test',
+           annee:'1992',
+           description:'42',
+        }
+    ])
+
+    const handleSubmit=(e)=>{
+        e.preventDefault();
+        setEditMode(false);
+    }
+    const handleEdit=()=>{
+        setEditMode(true);
+    }
+    const handleChange=(index,key,value)=>{
+        const update=[...ecole];
+        update[index][key]=value;
+        setEcole(update);
+    }
+    const handleDelete=(index)=>{
+        const update=ecole.filter((_,i)=>i !== index);
+        setEcole(update);
+    }
+    const handleAdd=()=>{
+        const newEcole={
+            titre:'',
+            annee:'',
+            description:'',
+        };
+        setEcole([...ecole,newEcole]);
+        setEditMode(true);
+    }
     return(
         <div className="global">
-            <h2>Bonjour Je suis la partie XP Ecole</h2>
+            <h2> Ecole et Formation</h2>
+            {ecole.map((ecole,index)=>(
+                <div key={index}>
+                    {editMode ?(
+                        <form onSubmit={handleChange}></form>
+                    ):(
+                        <div>
+
+                        </div>
+                    )}
+                </div>
+            ))}
+            {editMode?(
+                <>
+                    <br/>
+                    <button type="submit" onClick={handleSubmit} className='btn'>Submit</button>
+                </>
+            ):(
+                <>
+                    <br/>
+                    <button onClick={handleEdit} className='btn'>Edit</button>
+                </>
+            )}
+            <button onClick={handleAdd} className='btnAdd'>Ajout New ECole / Formation </button>
         </div>
     )
 }
